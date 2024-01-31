dd4870870132bbbcc2238c1c1f255fc857eb4dad
greyBearClaw <apraxas85@gmail.com>
2024-01-31 10:08:31 +0900

    #2.3

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 2;
-const myName = "Jaehyun";
+let myName = "Jaehyun";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "JaehyunPark";
+
+console.log("your new name is " + myName);
