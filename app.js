1adef480b3fa4b12309bb6098f792539ba223795
greyBearClaw <apraxas85@gmail.com>
2024-01-31 11:19:10 +0900

    #2.5

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const amIFat = true;
-console.log(amIFat);
+// array를 사용하여 살 것 리스트 만들고 피자 선택하기
+const toBuy = ["potate", "tomato", "pizza", "beer"];
+console.log(toBuy[2]);
 
-const amISkinny = false;
-console.log(amISkinny);
-
-let amIChage = null;
-console.log(amIChage);
-
-let something;
-console.log(something);
+// array 리스트에 OhYes! 추가하고 전체 목록 출력
+toBuy.push("ohYes!");
+console.log(toBuy);
