1dac25c104db534fd8b5f1b9de221c402502a00b
greyBearClaw <apraxas85@gmail.com>
2024-02-01 09:22:42 +0900

    #2.6

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// array를 사용하여 살 것 리스트 만들고 피자 선택하기
-const toBuy = ["potate", "tomato", "pizza", "beer"];
-console.log(toBuy[2]);
-
-// array 리스트에 OhYes! 추가하고 전체 목록 출력
-toBuy.push("ohYes!");
-console.log(toBuy);
+const player = {
+  name: "Gray Bear",
+  points: 10,
+  handsome: false,
+  fat: true,
+};
+console.log(player);
+player.lastName = "Claw";
+player.points = player.points + 15;
+console.log(player);
