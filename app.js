a129116f112dd192268449d7388ee5e17c1e86e9
greyBearClaw <apraxas85@gmail.com>
2024-02-01 09:49:54 +0900

    #2.7

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const player = {
-  name: "Gray Bear",
-  points: 10,
-  handsome: false,
-  fat: true,
-};
-console.log(player);
-player.lastName = "Claw";
-player.points = player.points + 15;
-console.log(player);
+// function은 계속 반복해서 사용할 수 있는 코드 조각
+// function은 ( )가 필요
+
+function sayHello() {
+  console.log("Hello! My name is C");
+}
+
+sayHello("Gray Bear");
+sayHello("Red Bear");
+sayHello("Brown Bear");
