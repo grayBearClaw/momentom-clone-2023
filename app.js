885461fa4eddda161cca838e8d371cd4c3bc24e8
greyBearClaw <apraxas85@gmail.com>
2024-02-03 09:22:18 +0900

    #2.13

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiply: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  square: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(5, 10);
-const minusResult = calculator.minus(plusResult, 5);
-const multiplyResult = calculator.multiply(minusResult, 3);
-const divideResult = calculator.divide(multiplyResult, 2);
-const squareResult = calculator.square(divideResult, 2);
-
-console.log(plusResult);
-console.log(minusResult);
-console.log(multiplyResult);
-console.log(divideResult);
-console.log(squareResult);
+console.log(age);
