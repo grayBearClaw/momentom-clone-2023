4cac3af194346df2668b81836cac7c515a4462b4
greyBearClaw <apraxas85@gmail.com>
2024-03-04 17:07:50 +0900

    #css.2

--- js/background.js
+++ js/background.js
@@ -1,13 +1,7 @@
-const images = ["01.jpg", "02.jpg", "03.jpg"];
+const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const imageUrl = `url(img/${chosenImage})`;
+const bgImageUrl = `url(img/${chosenImage})`;
 
-document.body.style.backgroundImage = imageUrl;
-
-// const bgImage = document.createElement("img");
-
-// bgImage.src = `img/${chosenImage}`;
-
-// document.body.appendChild(bgImage);
+document.body.style.backgroundImage = bgImageUrl;
